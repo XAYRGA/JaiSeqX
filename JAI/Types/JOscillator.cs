df408ef8e369f1247b1e7caf627c62bc8e26d86b
XAYRGA <code@xayr.ga>
2019-09-17 16:54:03 -0400

    Update JOscillator.cs

--- JAI/Types/JOscillator.cs
+++ JAI/Types/JOscillator.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace JaiSeqX.JAI.Types
 {
-    class JOscillator
+    public class JOscillator
     {
+        public byte target;
+        public float rate;
+        public JOscillatorVector[] vectors;
+        public float Width;
+        public float Vertex;
+    }
+
+    public class JOscillatorVector
+    {
+        public short mode;
+        public short time;
+        public short value;
+    }
+
+    public enum JOscillatorVectorMode
+    {
+        Linear = 1,
+        Square = 2,
+        SquareRoot = 3,
+        SampleCell = 4,
+        Loop = 5,
+        Hold = 6,
+        Stop = 7,
     }
 }
